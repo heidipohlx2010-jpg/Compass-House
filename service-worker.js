5f8641201e77ae96bcdfe1a9b043654aa6d2522b
heidipohlx2010-jpg <heidipohlx2010@gmail.com>
2025-08-18 14:26:11 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
 const CACHE='compass-house-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const ASSETS=[
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest'
+];
 
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
@@ -7,10 +13,14 @@ self.addEventListener('install', e=>{
 
 self.addEventListener('activate', e=>{
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys=>
+      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res=>res || fetch(e.request))
+  );
 });
